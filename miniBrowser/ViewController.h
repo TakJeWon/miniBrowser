baf3945cbf21069e73b8cf3465737fe16736c3e5
TakJeWon <taknineball@castis.com>
2022-01-17 17:49:49 +0900

    mini browser project with Objective-C

--- miniBrowser/ViewController.h
+++ miniBrowser/ViewController.h
@@ -6,8 +6,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate, WKNavigationDelegate>
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *bookMarkSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
+@property (weak, nonatomic) IBOutlet WKWebView *wkwebView;
+
+- (IBAction)bookmarkAction:(id)sender;
+
+- (IBAction)backAction:(id)sender;
+- (IBAction)forwardAction:(id)sender;
+- (IBAction)stopAction:(id)sender;
+- (IBAction)refreshAction:(id)sender;
 
 
 @end
--- miniBrowser/ViewController.m
+++ miniBrowser/ViewController.m
@@ -13,10 +13,65 @@
 
 @implementation ViewController
 
+@synthesize bookMarkSegmentedControl, urlTextField, wkwebView, activityIndicatorView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSString *urlString = @"http://www.facebook.com";
+    [wkwebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    urlTextField.text = urlString;
+
+    self.wkwebView.navigationDelegate = self;
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    NSString *urlString = urlTextField.text;
+    
+    if (![urlString hasPrefix:@"http://"]){
+        urlString = [[NSString alloc] initWithFormat:@"http://%@",urlString];
+    }
+    [wkwebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(IBAction)bookmarkAction:(id)sender{
+    
+    NSString *bookmarkTitle = [bookMarkSegmentedControl titleForSegmentAtIndex:bookMarkSegmentedControl.selectedSegmentIndex];
+    NSLog(@"%@", bookmarkTitle);
+    
+    NSString *urlString = [[NSString alloc] initWithFormat:@"http://www.%@.com",bookmarkTitle];
+    [wkwebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    urlTextField.text = urlString;
+}
+
+- (IBAction)refreshAction:(id)sender {
+    [wkwebView reload];
+}
+
+- (IBAction)stopAction:(id)sender {
+    [wkwebView stopLoading];
+}
+
+- (IBAction)forwardAction:(id)sender {
+    [wkwebView goForward];
+}
+
+- (IBAction)backAction:(id)sender {
+    [wkwebView goBack];
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation{
+    [activityIndicatorView startAnimating];
+}
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+    [activityIndicatorView stopAnimating];
+}
+
 
 
 @end
